f573907ffd59650713b5afe53e11dc1d0b9d8e6f
Dopeycylender39 <briggsjjacob@gmail.com>
2023-01-12 11:44:57 -0700

    r

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 let date = new Date();
 let year = date.getFullYear();
 
-// document.querySelector('h1').innerHTML = `${year} &copy`;
+document.querySelector('h1').innerHTML = `${year} &copy`;
 
 let currentdate = document.lastModified;
-document.querySelector('div').textContent = `Last updated: ${currentdate} &copy ${year}`;
+document.querySelector('div').textContent = `Last updated: ${currentdate}`;
 /* test test */ 
