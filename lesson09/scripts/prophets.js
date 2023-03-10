adf4512c6f435bdfd1da7e1db9b511df9bb6e756
Dopeycylender39 <briggsjjacob@gmail.com>
2023-03-10 12:47:25 -0700

    commit : )

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,37 +1,64 @@
-const url =
-  "https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json";
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 
 async function getProphetData() {
-  const response = await fetch(url);
-  const data = await response.json();
-  console.table(data.prophets); // note that we reference the prophet array of the data object given the structure of the json file
+    const response = await fetch(url);
+    const data = await response.json();
+    console.table(data.prophets);  // note that we reference the prophet array of the data object given thestructure of the json file
+    // displayProphets(data.prophets);
+    displayTable(data.prophets);
+
+}
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards'); // select the output container element
+  
+    prophets.forEach((prophet) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let portrait = document.createElement('img');
+      let birthdate = document.createElement('p');
+      let birthplace = document.createElement('p');
+  
+      // Build the h2 content out to show the prophet's full name - finish the template string
+      h2.textContent = `${prophet.name} ${prophet.lastname}`;
+      birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+      birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+  
+      // Build the image portrait by setting all the relevant attribute
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(h2);
+      card.appendChild(birthdate);
+      card.appendChild(birthplace);
+      card.appendChild(portrait);
+  
+      cards.appendChild(card);
+    } // end of forEach loop
+  )} // end of function expression
+
+function displayTable(prophets) {
+  prophets.forEach((prophet) => {
+    let tr = document.createElement('tr');
+    let td_name = document.createElement('td');
+    let td_birthplace = document.createElement('td');
+    let td_birthdate = document.createElement('td');
+
+    td_name.textContent = `${prophet.name} ${prophet.lastname}`;
+    td_birthplace.textContent = prophet.birthplace;
+    td_birthdate.textContent = prophet.birthdate;
+
+    tr.appendChild(td_name);
+    tr.appendChild(td_birthplace);
+    tr.appendChild(td_birthdate);
+
+    document.querySelector('table').appendChild(tr);
+  })
 }
 
 getProphetData();
-
-const displayProphets = (prophets) => {
-  const cards = document.querySelector("div.cards"); // select the output container element
-
-  prophets.forEach(
-    (prophet) => {
-      // Create elements to add to the div.cards element
-      let card = document.createElement("section");
-      let h2 = document.createElement("h2");
-      let portrait = document.createElement("img");
-
-      // Build the h2 content out to show the prophet's full name - finish the template string
-      h2.textContent = `${prophet.name} ____________`;
-
-      // Build the image portrait by setting all the relevant attribute
-      portrait.setAttribute("src", prophet.imageurl);
-      portrait.setAttribute("alt", `Portait of ${prophet.name} ______________`);
-
-      // Append the section(card) with the created elements
-      card.appendChild(h2);
-      card.appendChild(portrait);
-      cards.appendChild(card);
-    } // end of forEach loop
-  );
-}; // end of function expression
-
-// function displayTable
